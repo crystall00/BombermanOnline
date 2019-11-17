51df0ef78af5db01d3f4480973f4f007dfd63e1b
Anton Markaj <anton.markaj@students.ffhs.ch>
2019-11-17 16:51:20 +0100

    Replaced Bootstrap with CSS Grid

--- client/js/siteBuilder.js
+++ client/js/siteBuilder.js
@@ -1,23 +1,3 @@
 $(function () {
-    let gameSection = $('game');
-    let bootstrapRow = document.createElement('div');
-    $(bootstrapRow).addClass('row edge no-gutter ').appendTo($("#game"));
-    for (let i = 0; i < 13; i++) {
-        let bootstrapRow = document.createElement('div');
-        $(bootstrapRow).addClass('row no-gutter').appendTo($("#game"));
-        let bootstrapColumn = document.createElement('div');
-        $(bootstrapColumn).addClass('col edge').appendTo($(bootstrapRow));
-        for (let j = 0; j < 13; j++) {
-            let bootstrapColumn = document.createElement('div');
-            if (i % 2 !== 0 && j % 2 !== 0) {
-                $(bootstrapColumn).addClass('col edge').appendTo($(bootstrapRow));
-            } else {
-                $(bootstrapColumn).addClass('col').appendTo($(bootstrapRow));
-            }
-        }
-        bootstrapColumn = document.createElement('div');
-        $(bootstrapColumn).addClass('col edge').appendTo($(bootstrapRow));
-    }
-    bootstrapRow = document.createElement('div');
-    $(bootstrapRow).addClass('row edge no-gutter ').appendTo($("#game"));
+
 });
--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var sass = require('gulp-sass');
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function () {
-    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'client/scss/*.scss'])
+    return gulp.src(['client/scss/*.scss'])
         .pipe(sass())
         .pipe(gulp.dest("client/css"))
         .pipe(browserSync.stream());
@@ -12,7 +12,7 @@ gulp.task('sass', function () {
 
 // Move the javascript files into our /client/js folder
 gulp.task('js', function () {
-    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
+    return gulp.src(['node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js', 'client/js/*.js'])
         .pipe(gulp.dest("client/js"))
         .pipe(browserSync.stream());
 });
@@ -24,7 +24,7 @@ gulp.task('serve', ['sass'], function () {
         server: "./client"
     });
 
-    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'client/scss/*.scss'], ['sass']);
+    gulp.watch(['client/scss/*.scss'], ['sass']);
     gulp.watch("client/*.html").on('change', browserSync.reload);
 });
 
