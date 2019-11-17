730cdf38ef16ec30d60082bcd106f79908242ce7
Anton Markaj <anton.markaj@students.ffhs.ch>
2019-11-17 19:33:17 +0100

    Recreated Sitebuilder to build Game Ground

--- client/js/siteBuilder.js
+++ client/js/siteBuilder.js
@@ -1,3 +1,16 @@
 $(function () {
-
+    let gameContainer = $('#game');
+    for (let i = 0; i < 15; i++) {
+        for (let j = 0; j < 15; j++) {
+            let divElement = document.createElement('div');
+            divElement.id = i.toString() + "_" + j.toString();
+            if (i === 0 || j === 0 || i === 14 || j === 14) {
+                $(divElement).addClass('wall').appendTo($("#game"));
+            } else if ((i % 2 === 0 && j % 2 === 0)) {
+                $(divElement).addClass('wall').appendTo($("#game"));
+            } else {
+                $(divElement).appendTo($("#game"));
+            }
+        }
+    }
 });
