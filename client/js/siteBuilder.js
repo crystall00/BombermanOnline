a2001bca624ac17d51ad4a2ed9ea8754eaf141c3
Anton Markaj <anton.markaj@students.ffhs.ch>
2019-11-18 21:27:45 +0100

    Updated UI elements

--- client/js/siteBuilder.js
+++ client/js/siteBuilder.js
@@ -6,10 +6,12 @@ function resetGame() {
         for (let j = 0; j < 15; j++) {
             let divElement = document.createElement('div');
             divElement.id = i.toString() + "_" + j.toString();
-            if (i === 0 || j === 0 || i === 14 || j === 14) {
+            if (i === 0 || i === 14) {
                 $(divElement).addClass('wall').appendTo($("#game"));
+            } else if ((j === 0 || j === 14) && (i !== 0 || i !== 14)) {
+                $(divElement).addClass('wallVertical').appendTo($("#game"));
             } else if ((i % 2 === 0 && j % 2 === 0)) {
-                $(divElement).addClass('wall').appendTo($("#game"));
+                $(divElement).addClass('block').appendTo($("#game"));
             } else if (!((i < 3 || i > 11) && (j < 3 || j > 11))) {
                 $(divElement).addClass('ice').appendTo($("#game"));
             } else {
